a864e72f472e78aea7814cd6cc9740485df58df1
Natália Guimaraes <nataliaguimaraes@macbook-air-de-natalia.local>
2018-05-01 12:14:22 -0300

    Initial Commit

---
+++ TH01Q03/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TH01Q03
+//
+//  Created by Natália Guimaraes on 01/05/2018.
+//  Copyright © 2018 Natália Guimaraes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
